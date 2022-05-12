04da28a8db0484033501bd4f25d4d4a7f5c41b96
halil <halilerk77@icloud.com>
2022-05-12 23:47:01 +0300

    finished

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,13 @@
 //! FİBONACCİ NUMBER //
 
-let fibo = [0, 1, 1];
-let sayi = 10;
-for (let i = 0; i < sayi; i++) {
-  newFibo = fibo[fibo.length - 1] + fibo[fibo.length - 2];
-  fibo.push(newFibo);
+let fibo = [0, 1];
+let sayi = Number(prompt("Please Enter a Number"));
+
+function halil() {
+  for (let i = 0; i < sayi - 2; i++) {
+    let newFibo = fibo[fibo.length - 1] + fibo[fibo.length - 2];
+    fibo.push(newFibo);
+  }
+  return fibo;
 }
-console.log(fibo);
+console.log(halil());
