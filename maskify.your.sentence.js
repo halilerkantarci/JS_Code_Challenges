b31215a958ad7b69d82ca13e1c8e083d56f7a010
halil <halilerk77@icloud.com>
2022-05-13 17:13:08 +0300

    last-version

--- maskify.your.sentence.js
+++ maskify.your.sentence.js
@@ -1,11 +1,18 @@
+//! FİRST WAY
+
 let text = "what was the name of your first pet?";
 cc = "skippy";
 let liste1 = [];
 let a = cc.split("");
-console.log(a);
+
 for (i = 0; i < a.length - 4; i++) {
   liste1.push("#");
 }
-console.log(liste1.join(""));
 let dd = liste1.join("") + a.join("").slice(-4);
 console.log(dd);
+
+//! SECOND WAY
+function maskify(cc) {
+  return cc.slice(0, -4).replace(/./g, "#") + cc.slice(-4);
+}
+console.log(maskify("skippy"));
