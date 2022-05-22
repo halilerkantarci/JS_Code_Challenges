3b1368daca9f1dce067d7eed92f170f507bfaf88
halil <halilerk77@icloud.com>
2022-05-22 15:33:54 +0300

    chain me

---
+++ 14-chain-me.js
@@ -0,0 +1,22 @@
+// Write a generic function chainer
+// Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+
+// The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). Return the final value after execution is complete.
+// function add(num) {
+//     return num + 1;
+//   }
+
+//   function mult(num) {
+//     return num * 30;
+//   }
+
+//   chain(2, [add, mult]);
+//! returns 90;
+
+function chainMe(input, fs) {
+  let output = input;
+  for (let i = 0; i < fs.length; i++) {
+    output = fs[i](output);
+  }
+  return output;
+}
