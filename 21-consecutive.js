41c633346337dace3c82a3a3c59ac2f45af64321
halil <halilerk77@icloud.com>
2022-05-23 15:22:41 +0300

    consecutive

---
+++ 21-consecutive.js
@@ -0,0 +1,23 @@
+// strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+// Concatenate the consecutive strings of strarr by 2, we get:
+
+// treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so
+// longest_consec(strarr, 2) should return "folingtrashy".
+
+// In the same way:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+(strarr = ["tree", "fosssssling", "trashy", "blue", "abcdef", "uvwxyzds"]),
+  (k = 2);
+let halil = [];
+for (let i = 0; i < strarr.length - 1; i++) {
+  halil.push(strarr[i].concat(strarr[i + 1]));
+}
+console.log(halil.sort((a, b) => b.length - a.length)[0]);
