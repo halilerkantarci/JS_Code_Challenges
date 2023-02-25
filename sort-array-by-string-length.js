d4ea2635b6de00b7d80f13b16e3c944daec415ff
halil <halilerk77@icloud.com>
2023-02-26 00:08:18 +0100

    js

--- sort-array-by-string-length.js
+++ sort-array-by-string-length.js
@@ -15,4 +15,4 @@ const array = ["Telescopes", "Glasses", "Eyes", "Monocles"];
 function sortByLength(array) {
   return array.sort((a, b) => a.length - b.length);
 }
-console.log(sortByLength(array));
+console.log(sortByLength(array)); // ['Eyes', 'Glasses', 'Monocles', 'Telescopes']
